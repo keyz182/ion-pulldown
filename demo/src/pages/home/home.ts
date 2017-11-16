bc13f8579b6a7a3dfd223d85184d49d62a539e10
Kieran David Evans <keyz182@gmail.com>
2017-11-16 13:56:43 +0000

    V0.0.4 New callback functionality, fix expand/collapse external call

--- demo/src/pages/home/home.ts
+++ demo/src/pages/home/home.ts
@@ -1,21 +1,62 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild, Renderer2, ElementRef } from '@angular/core';
+import { NavController, List } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild('list') list: List ;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public renderer:Renderer2, public elRef:ElementRef) {
 
   }
 
-  onMove() {
+  MoveStart() {
+    console.log('onMoveStart');
+    let el = this.elRef.nativeElement.querySelector('#onMoveStart');
+    this.fadeOut(el);
+  }
+
+  Move() {
     console.log('onMove');
+    let el = this.elRef.nativeElement.querySelector('#onMove');
+    this.fadeOut(el);
   }
 
-  onMoveDone() {
+  MoveDone() {
     console.log('onMoveDone');
+    let el = this.elRef.nativeElement.querySelector('#onMoveDone');
+    this.fadeOut(el);
+  }
+
+  Init() {
+    console.log('onInit');
+    let el = this.elRef.nativeElement.querySelector('#onInit');
+    this.fadeOut(el);
+  }
+
+  Collapse() {
+    console.log('onCollapse');
+    let el = this.elRef.nativeElement.querySelector('#onCollapse');
+    this.fadeOut(el);
+  }
+
+  Expand() {
+    console.log('onExpand');
+    let el = this.elRef.nativeElement.querySelector('#onExpand');
+    this.fadeOut(el);
+  }
+
+  MidPoint() {
+    console.log('onMidPoint');
+    let el = this.elRef.nativeElement.querySelector('#onMidPoint');
+    this.fadeOut(el);
+  }
+
+  fadeOut(el: HTMLElement){
+    let clone:HTMLElement = <HTMLElement>el.cloneNode(true);
+    clone.classList.remove('hidden');
+    el.parentNode.replaceChild(clone, el);
   }
 }
--- src/pulldown.ts
+++ src/pulldown.ts
@@ -1,254 +1,288 @@
 import {
-  ChangeDetectionStrategy,
-  Component,
-  ElementRef,
-  EventEmitter,
-  Input,
-  Output,
-  ViewChild,
-  Renderer2,
-  OnInit,
-  ContentChild
+    ChangeDetectionStrategy,
+    Component,
+    ContentChild,
+    ElementRef,
+    EventEmitter,
+    Input,
+    OnInit,
+    Output,
+    Renderer2,
+    ViewChild
 } from '@angular/core';
-import {Platform, Content} from 'ionic-angular';
+import {Content, Platform} from 'ionic-angular';
 import 'rxjs/Rx';
 import {IonPulldownHeaderDirective} from './header-wrap';
 import {IonPulldownTabComponent} from './pulldown-tab';
 
 @Component({
-  selector: 'ion-pull-down',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  template: `
-  <ion-header ionPulldownHeader #header>
-    <ng-content></ng-content>
-    <ion-pulldown-tab></ion-pulldown-tab>
-  </ion-header>
-  `
+    selector: 'ion-pull-down',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    template: `
+        <ion-header ionPulldownHeader #header>
+            <ng-content></ng-content>
+            <ion-pulldown-tab></ion-pulldown-tab>
+        </ion-header>
+    `
 })
-export class IonPullDownComponent  implements OnInit{
-  @ContentChild(Content) public content: Content;
-  @ViewChild(IonPulldownHeaderDirective) header: IonPulldownHeaderDirective;
-  @ViewChild(IonPulldownHeaderDirective, {read: ElementRef}) headerRef: ElementRef;
-  @ViewChild(IonPulldownTabComponent) tab: IonPulldownTabComponent;
-  @ViewChild(IonPulldownTabComponent, {read: ElementRef}) tabRef: ElementRef;
+export class IonPullDownComponent implements OnInit {
+    @ContentChild(Content) public content: Content;
+    @ViewChild(IonPulldownHeaderDirective) header: IonPulldownHeaderDirective;
+    @ViewChild(IonPulldownHeaderDirective, {read: ElementRef}) headerRef: ElementRef;
+    @ViewChild(IonPulldownTabComponent) tab: IonPulldownTabComponent;
+    @ViewChild(IonPulldownTabComponent, {read: ElementRef}) tabRef: ElementRef;
 
-  @Input() mainContent: Content;
+    @Input() mainContent: Content;
 
-  @Output() onMove: EventEmitter<any> = new EventEmitter<any>();
-  @Output() onMoveDone: EventEmitter<any> = new EventEmitter<any>();
+    @Output() onInit: EventEmitter<any> = new EventEmitter<any>();
+    @Output() onMoveStart: EventEmitter<any> = new EventEmitter<any>();
+    @Output() onMove: EventEmitter<any> = new EventEmitter<any>();
+    @Output() onMoveDone: EventEmitter<any> = new EventEmitter<any>();
+    @Output() onCollapse: EventEmitter<any> = new EventEmitter<any>();
+    @Output() onExpand: EventEmitter<any> = new EventEmitter<any>();
+    @Output() onMidPoint: EventEmitter<any> = new EventEmitter<any>();
 
-  private defaultHeight: number;
-  private maxHeight: number;
-  private currentHeight: number;
-  private minHeight: number;
-  private footer: Element;
-  private footerHeight: number;
-  private toolbar: Element;
-  private toolbarHeight: number;
-  private tabHeight: number;
-  private mainContentHeight: number;
+    private defaultHeight: number;
+    private maxHeight: number;
+    private currentHeight: number;
+    private minHeight: number;
+    private footer: Element;
+    private footerHeight: number;
+    private toolbar: Element;
+    private toolbarHeight: number;
+    private tabHeight: number;
+    private mainContentHeight: number;
 
-  private previousPosY: number;
-  private currentPosY: number;
+    private previousPosY: number;
+    private currentPosY: number;
 
-  private hammer;
+    private hammer;
 
-  constructor(private platform: Platform, private renderer: Renderer2){
-    if(!window.hasOwnProperty('Hammer')){
-      throw new Error('Hammer.js not found at window.Hammer!');
-    }else{
-      this.hammer = window['Hammer'];
-    }
-  }
-
-  ngOnInit(): void {
-    const component = this;
-
-    window.addEventListener("orientationchange", () => {
-        component.updateUI();
-    });
-      component.platform.resume.subscribe(() => {
-        component.updateUI();
-    });
-
-    let hammerOpts = {};
-
-    if(navigator.userAgent.match(/Android/i)) {
-      hammerOpts['inputClass'] = window['Hammer'].TouchInput;
+    constructor(private platform: Platform, private renderer: Renderer2) {
+        if (!window.hasOwnProperty('Hammer')) {
+            throw new Error('Hammer.js not found at window.Hammer!');
+        } else {
+            this.hammer = window['Hammer'];
+        }
     }
 
-    function handler(event) {
+    ngOnInit(): void {
+        const component = this;
 
-      switch(event.type){
-        case 'panstart':
-          component.renderer.removeStyle(component.headerRef.nativeElement, 'transition');
-          component.renderer.removeStyle(component.headerRef.nativeElement, '-o-transition');
-          component.renderer.removeStyle(component.headerRef.nativeElement, '-ms-transition');
-          component.renderer.removeStyle(component.headerRef.nativeElement, '-moz-transition');
-          component.renderer.removeStyle(component.headerRef.nativeElement, '-webkit-transition');
+        window.addEventListener("orientationchange", () => {
+            component.updateUI();
+        });
+        component.platform.resume.subscribe(() => {
+            component.updateUI();
+        });
 
-          component.renderer.removeStyle(component.content.getNativeElement(), 'display');
+        let hammerOpts = {};
 
-          component.content.resize();
-          component.mainContent.resize();
-          component.onMove.emit();
-          break;
-        case 'pan':
-        case 'pan-up':
-        case 'pan-down':
-          event.preventDefault();
+        if (navigator.userAgent.match(/Android/i)) {
+            hammerOpts['inputClass'] = window['Hammer'].TouchInput;
+        }
 
-          let deltaY = event.deltaY;
-          let xlate = component.previousPosY + deltaY - 4;
+        function handler(event) {
 
-          if(xlate < component.minHeight) xlate = component.minHeight;
-          if(xlate > component.maxHeight) xlate = component.maxHeight;
+            switch (event.type) {
+                case 'panstart':
+                    component.startStyle();
 
-          component.currentPosY = xlate;
+                    component.content.resize();
+                    component.mainContent.resize();
+                    component.onMoveStart.emit();
+                    break;
+                case 'pan':
+                case 'pan-up':
+                case 'pan-down':
+                    event.preventDefault();
 
-          component.clearStyle();
-          component.setStyle(xlate, true);
+                    let deltaY = event.deltaY;
+                    let xlate = component.previousPosY + deltaY - 4;
 
-          component.content.resize();
-          component.mainContent.resize();
-          component.onMove.emit();
-          break;
-        case 'panend':
-          component.renderer.removeStyle(component.headerRef.nativeElement, 'transition');
-          component.renderer.setStyle(component.headerRef.nativeElement, 'transition', '300ms ease-in-out');
+                    if (xlate < component.minHeight) xlate = component.minHeight;
+                    if (xlate > component.maxHeight) xlate = component.maxHeight;
 
-          if(component.currentPosY > (component.maxHeight * 0.8)) {
-            component.expand();
-          }else if(component.currentPosY < (component.maxHeight * 0.2)){
-            component.collapse();
-          }else {
-            component.currentPosY = component.maxHeight/2;
+                    component.currentPosY = xlate;
 
-            component.clearStyle();
-            component.setStyle(component.currentPosY);
-          }
+                    component.clearStyle();
+                    component.setStyle(xlate, true);
 
-          component.previousPosY = component.currentPosY;
-          component.content.resize();
-          component.mainContent.resize();
-          component.onMoveDone.emit();
+                    component.content.resize();
+                    component.mainContent.resize();
+                    component.onMove.emit();
+                    break;
+                case 'panend':
+                    component.endStyle();
 
-          break;
-        default:
-          break;
-      }
+                    if (component.currentPosY > (component.maxHeight * 0.8)) {
+                        component.expand();
+                    } else if (component.currentPosY < (component.maxHeight * 0.2)) {
+                        component.collapse();
+                    } else {
+                        component.midPoint();
+                    }
+
+                    component.previousPosY = component.currentPosY;
+                    component.content.resize();
+                    component.mainContent.resize();
+                    component.onMoveDone.emit();
+
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        let hammer = new this.hammer(this.tabRef.nativeElement, hammerOpts);
+
+        hammer.get('pan').set({threshold: 0});
+
+        hammer.on('pan panstart panend pan-up pan-down', handler);
+
+        this.updateUI(true);
     }
 
-    let hammer = new this.hammer(this.tabRef.nativeElement, hammerOpts);
+    getHeights() {
+        this.defaultHeight = this.headerRef.nativeElement.offsetHeight;
+        this.footer = document.querySelector('ion-footer');
+        this.footerHeight = (<HTMLElement>this.footer).offsetHeight;
+        this.toolbar = document.querySelector('ion-navbar');
+        if (!this.toolbar) {
+            this.toolbar = document.querySelector('ion-toolbar');
+        }
+        this.toolbarHeight = (<HTMLElement>this.toolbar).offsetHeight;
+        this.tabHeight = this.tabRef.nativeElement.offsetHeight;
+        this.minHeight = 0;
+        this.currentHeight = this.minHeight;
+        this.maxHeight = window.innerHeight - this.footerHeight - this.toolbarHeight - this.tabHeight;
+        this.mainContentHeight = window.innerHeight - this.toolbarHeight;
 
-    hammer.get('pan').set({threshold: 0});
-
-    hammer.on('pan panstart panend pan-up pan-down', handler);
-
-    this.updateUI(true);
-  }
-
-  getHeights(){
-    this.defaultHeight = this.headerRef.nativeElement.offsetHeight;
-    this.footer = document.querySelector('ion-footer');
-    this.footerHeight = (<HTMLElement>this.footer).offsetHeight;
-    this.toolbar = document.querySelector('ion-navbar');
-    if(!this.toolbar){
-        this.toolbar = document.querySelector('ion-toolbar');
-    }
-    this.toolbarHeight = (<HTMLElement>this.toolbar).offsetHeight;
-    this.tabHeight = this.tabRef.nativeElement.offsetHeight;
-    this.minHeight = 0;
-    this.currentHeight = this.minHeight;
-    this.maxHeight = window.innerHeight - this.footerHeight - this.toolbarHeight - this.tabHeight;
-    this.mainContentHeight = window.innerHeight - this.toolbarHeight;
-
-    this.renderer.setStyle(this.mainContent.getNativeElement(), 'height', this.mainContentHeight + 'px');
-    this.renderer.setStyle(this.mainContent.getNativeElement(),
-      'transform',
-      'translate3d(0,' + (this.toolbarHeight) + 'px, 0)');
-    this.renderer.setStyle(this.mainContent.getNativeElement(),
-      'transition',
-      'none');
-  }
-
-  computeHeights(isInit: boolean = false){
-    this.getHeights();
-    this.content.resize();
-
-    this.renderer.setStyle(this.headerRef.nativeElement, 'height', this.currentHeight + 'px');
-
-    this.collapse(isInit);
-  }
-
-  updateUI(isInit: boolean = false) {
-    setTimeout(() => {
-      this.computeHeights(isInit);
-    }, 10);
-    // avoids flickering when changing orientation
-    this.renderer.setStyle(this.headerRef.nativeElement, 'transition', 'none');
-  }
-
-  clearStyle(clearTransition: boolean = true){
-    if(clearTransition){
-      this.renderer.removeStyle(this.headerRef.nativeElement, 'transition');
-      this.renderer.removeStyle(this.headerRef.nativeElement, '-o-transition');
-      this.renderer.removeStyle(this.headerRef.nativeElement, '-ms-transition');
-      this.renderer.removeStyle(this.headerRef.nativeElement, '-moz-transition');
-      this.renderer.removeStyle(this.headerRef.nativeElement, '-webkit-transition');
+        this.renderer.setStyle(this.mainContent.getNativeElement(), 'height', this.mainContentHeight + 'px');
+        this.renderer.setStyle(this.mainContent.getNativeElement(),
+            'transform',
+            'translate3d(0,' + (this.toolbarHeight) + 'px, 0)');
+        this.renderer.setStyle(this.mainContent.getNativeElement(),
+            'transition',
+            'none');
     }
 
-    this.renderer.removeStyle(this.headerRef.nativeElement, 'height');
-    this.renderer.removeStyle(this.mainContent.getNativeElement(), 'height');
-    this.renderer.removeStyle(this.mainContent.getNativeElement(),'transform');
-  }
+    computeHeights(isInit: boolean = false) {
+        this.getHeights();
+        this.content.resize();
 
-  setStyle(height: number, transition: boolean = false){
-    this.renderer.setStyle(this.headerRef.nativeElement, 'height', height + 'px');
+        this.renderer.setStyle(this.headerRef.nativeElement, 'height', this.currentHeight + 'px');
 
-    this.renderer.setStyle(this.mainContent.getNativeElement(),
-      'height', (this.mainContentHeight - height ) + 'px');
-    this.renderer.setStyle(this.mainContent.getNativeElement(),
-      'transform',
-      'translate3d(0,' + (height + this.footerHeight) + 'px, 0)');
-
-    if(transition){
-      this.renderer.setStyle(this.headerRef.nativeElement, 'transition', 'height 0');
-      this.renderer.setStyle(this.headerRef.nativeElement, '-o-transition', 'height 0');
-      this.renderer.setStyle(this.headerRef.nativeElement, '-ms-transition', 'height 0');
-      this.renderer.setStyle(this.headerRef.nativeElement, '-moz-transition', 'height 0');
-      this.renderer.setStyle(this.headerRef.nativeElement, '-webkit-transition', 'height 0');
-    }
-  }
-
-  collapse(isInit: boolean = false){
-    this.currentPosY = 0;
-    this.previousPosY = this.minHeight;
-
-    this.clearStyle();
-    this.setStyle(this.minHeight, true);
-    this.renderer.setStyle(this.content.getNativeElement(), 'display', 'none');
-
-    this.content.resize();
-    this.mainContent.resize();
-
-    if(!isInit){
-      this.onMove.emit();
+        this.collapse(isInit);
     }
 
-    console.log('Collapse!');
-  }
+    updateUI(isInit: boolean = false) {
+        setTimeout(() => {
+            this.computeHeights(isInit);
+        }, 10);
+        // avoids flickering when changing orientation
+        this.renderer.setStyle(this.headerRef.nativeElement, 'transition', 'none');
+    }
 
-  expand() {
-    this.currentPosY = this.maxHeight;
-    this.previousPosY = this.maxHeight;
-    this.clearStyle();
-    this.setStyle(this.maxHeight, true);
+    clearStyle(clearTransition: boolean = true) {
+        if (clearTransition) {
+            this.renderer.removeStyle(this.headerRef.nativeElement, 'transition');
+            this.renderer.removeStyle(this.headerRef.nativeElement, '-o-transition');
+            this.renderer.removeStyle(this.headerRef.nativeElement, '-ms-transition');
+            this.renderer.removeStyle(this.headerRef.nativeElement, '-moz-transition');
+            this.renderer.removeStyle(this.headerRef.nativeElement, '-webkit-transition');
+        }
 
-    this.content.resize();
-    this.mainContent.resize();
-    this.onMove.emit();
+        this.renderer.removeStyle(this.headerRef.nativeElement, 'height');
+        this.renderer.removeStyle(this.mainContent.getNativeElement(), 'height');
+        this.renderer.removeStyle(this.mainContent.getNativeElement(), 'transform');
+    }
 
-    console.log('Expand!');
-  }
+    setStyle(height: number, transition: boolean = false) {
+        this.renderer.setStyle(this.headerRef.nativeElement, 'height', height + 'px');
+
+        this.renderer.setStyle(this.mainContent.getNativeElement(),
+            'height', (this.mainContentHeight - height ) + 'px');
+        this.renderer.setStyle(this.mainContent.getNativeElement(),
+            'transform',
+            'translate3d(0,' + (height + this.footerHeight) + 'px, 0)');
+
+        if (transition) {
+            this.renderer.setStyle(this.headerRef.nativeElement, 'transition', 'height 0');
+            this.renderer.setStyle(this.headerRef.nativeElement, '-o-transition', 'height 0');
+            this.renderer.setStyle(this.headerRef.nativeElement, '-ms-transition', 'height 0');
+            this.renderer.setStyle(this.headerRef.nativeElement, '-moz-transition', 'height 0');
+            this.renderer.setStyle(this.headerRef.nativeElement, '-webkit-transition', 'height 0');
+        }
+    }
+
+    startStyle(){
+        this.renderer.removeStyle(this.headerRef.nativeElement, 'transition');
+        this.renderer.removeStyle(this.headerRef.nativeElement, '-o-transition');
+        this.renderer.removeStyle(this.headerRef.nativeElement, '-ms-transition');
+        this.renderer.removeStyle(this.headerRef.nativeElement, '-moz-transition');
+        this.renderer.removeStyle(this.headerRef.nativeElement, '-webkit-transition');
+
+        this.renderer.removeStyle(this.content.getNativeElement(), 'display');
+    }
+
+    endStyle(){
+        this.renderer.removeStyle(this.headerRef.nativeElement, 'transition');
+        this.renderer.setStyle(this.headerRef.nativeElement, 'transition', '300ms ease-in-out');
+    }
+
+    collapse(isInit: boolean = false) {
+        this.startStyle();
+        this.currentPosY = 0;
+        this.previousPosY = this.minHeight;
+
+        this.clearStyle();
+        this.setStyle(this.minHeight, true);
+        this.renderer.setStyle(this.content.getNativeElement(), 'display', 'none');
+
+        this.endStyle();
+
+        this.content.resize();
+        this.mainContent.resize();
+
+        if (!isInit) {
+            this.onMove.emit();
+            this.onCollapse.emit();
+        } else {
+            this.onInit.emit();
+        }
+    }
+
+    expand() {this.content.resize();
+        this.startStyle();
+        this.mainContent.resize();
+
+        this.currentPosY = this.maxHeight;
+        this.previousPosY = this.maxHeight;
+        this.clearStyle();
+        this.setStyle(this.maxHeight, true);
+
+        this.endStyle();
+
+        this.content.resize();
+        this.mainContent.resize();
+        this.onMove.emit();
+        this.onExpand.emit();
+    }
+
+    midPoint() {
+        this.startStyle();
+        this.currentPosY = this.maxHeight / 2;
+        this.previousPosY = this.maxHeight / 2;
+
+        this.clearStyle();
+        this.setStyle(this.currentPosY);
+
+        this.endStyle();
+
+        this.content.resize();
+        this.mainContent.resize();
+        this.onMove.emit();
+        this.onMidPoint.emit();
+    }
 }
